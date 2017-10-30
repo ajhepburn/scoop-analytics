6e0f6d3bf17fadf8bbc83097f21a5f28b7805440
Alex <alex.j.hepburn@gmail.com>
2017-10-30 22:44:40 +0000

    Fix initialisation/connection errors

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup
 
 setup(
-    name='scoop-analytics',
-    packages=['scoop-analytics'],
+    name='scoop_analytics',
+    packages=['scoop_analytics'],
     include_package_data=True,
     install_requires=[
         'flask',
