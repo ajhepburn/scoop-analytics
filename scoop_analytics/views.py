eb6367f9e1a3a60e258e2c61c425d18ed7391863
Alex <alex.j.hepburn@gmail.com>
2017-10-30 22:44:40 +0000

    Fix initialisation/connection errors

--- scoop_analytics/views.py
+++ scoop_analytics/views.py
@@ -1,3 +1,7 @@
+from flask import render_template
+from scoop_analytics import app
+from scoop_analytics.models import db, BaseModel, Documents, SharePrices
+
 @app.route("/")
 def main():
     myDocuments = Documents.query.all()
