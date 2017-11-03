c11a7ddc68889db0b23d6601cb3ffaa359129964
Alex <alex.j.hepburn@gmail.com>
2017-11-03 19:55:58 +0000

    Basic visualisation, no data feed

--- scoop_analytics/views.py
+++ scoop_analytics/views.py
@@ -1,10 +1,16 @@
 from flask import render_template
 from scoop_analytics import app
 from scoop_analytics.models import db, BaseModel, Documents, SharePrices
+from sqlalchemy import *
+import time
 
 @app.route("/")
 def main():
-    myDocuments = Documents.query.all()
-    share_prices = SharePrices.query.all()
-    # print(share_prices)
-    return render_template('index.html', myDocuments=myDocuments, share_prices=share_prices)
+	myDocuments = Documents.query.limit(10).all()
+	share_prices = SharePrices.query.limit(10).all()
+
+	for el in share_prices:
+		tstamp_conv = time.strftime("%a, %d %b %Y %H:%M:%S", time.localtime(el.__dict__['timestamp']))
+		el.__dict__['timestamp'] = tstamp_conv
+
+	return render_template('index.html', myDocuments=myDocuments, share_prices=share_prices)
