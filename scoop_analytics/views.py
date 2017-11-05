08a217f25825f08a55e20c949f8fc4ce7ca07afa
Alex <alex.j.hepburn@gmail.com>
2017-11-05 23:13:25 +0000

    Implement basic visualisation

--- scoop_analytics/views.py
+++ scoop_analytics/views.py
@@ -10,7 +10,8 @@ def main():
 	share_prices = SharePrices.query.limit(10).all()
 
 	for el in share_prices:
-		tstamp_conv = time.strftime("%a, %d %b %Y %H:%M:%S", time.localtime(el.__dict__['timestamp']))
+		#tstamp_conv = time.strftime("%a, %d %b %Y %H:%M:%S", time.localtime(el.__dict__['timestamp']))
+		tstamp_conv = time.strftime("%H:%M", time.localtime(el.__dict__['timestamp']))
 		el.__dict__['timestamp'] = tstamp_conv
 
 	return render_template('index.html', myDocuments=myDocuments, share_prices=share_prices)
