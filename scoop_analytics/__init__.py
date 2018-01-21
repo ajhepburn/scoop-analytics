ef48e422b3c78c1212abb7dbfd6c89d78a8c147b
Alex <alex.j.hepburn@gmail.com>
2018-01-21 17:52:57 +0000

    Basic flask-dance twitter integration

--- scoop_analytics/__init__.py
+++ scoop_analytics/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask import render_template
+from flask_dance.contrib.twitter import make_twitter_blueprint, twitter
 from scoop_analytics.models import db, BaseModel, Documents, SharePrices
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'thisisasecret'
 
 import scoop_analytics.views
 
@@ -14,6 +16,9 @@ POSTGRES = {
     'host': 'localhost',
     'port': '5432',
 }
+
+twitter_blueprint = make_twitter_blueprint(api_key='7u1DrWrcqlRb3shnmSV271YAC', api_secret='BjP4LEUDaDp7oSg7H5P1i9jRPtDAnGWxN7dZCfPpqel2n7P4Mc')
+app.register_blueprint(twitter_blueprint, url_prefix='/twitter')
 
 app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://%(user)s:\
