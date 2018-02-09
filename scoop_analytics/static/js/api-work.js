54b9e408c9baa2e8b317e4dde96bcb2d5a9bfb7b
Alex <alex.j.hepburn@gmail.com>
2018-02-09 12:30:15 +0000

    Tweet panel CSS fix

--- scoop_analytics/static/js/api-work.js
+++ scoop_analytics/static/js/api-work.js
@@ -16,43 +16,50 @@ var twitterapi = {
 				    					.attr("id", function(d){return "p"+d['id_str']})
 				    					.classed("panel-body", true);
 
-					tweetDivs.append("img")
-						.attr("width", 20)
-						.attr("height", 20)
-						.attr("src", function(d){return d['user']['profile_image_url']})
-						.classed("panel-tweet-img-profile", true);
+					var headerBlock = tweetDivs.append("p")
+									  .classed("panel-tweet-header", true);
 
-					tweetDivs.append("p")
-						.text(function(d){
-							var tweet_created_format = d3.timeFormat("%-I:%M%p, %e %b %Y")(d3.timeParse("%a %b %d %H:%M:%S %Z %Y")(d['created_at']));
-							return "@"+d['user']['screen_name']+"    ("+tweet_created_format+")";
-						})
-						.classed("panel-tweet-text-header", true);
+						headerBlock.append("img")
+							.attr("width", 20)
+							.attr("height", 20)
+							.attr("src", function(d){return d['user']['profile_image_url']})
+							.classed("panel-tweet-img-profile", true);
 
-					tweetDivs.append("p")
-						.text(function(d){return d['text'];})
-						.classed("panel-tweet-text-body", true);
+						headerBlock.append("text")
+							.text(function(d){
+								var tweet_created_format = d3.timeFormat("%-I:%M%p, %e %b %Y")(d3.timeParse("%a %b %d %H:%M:%S %Z %Y")(d['created_at']));
+								return " @"+d['user']['screen_name']+"    ("+tweet_created_format+")";
+							})
+							.classed("panel-tweet-header-text", true);
 
+
+					var bodyBlock = tweetDivs.append("p")
+									.classed("panel-tweet-body", true);
+
+						bodyBlock.append("text")
+							.text(function(d){return d['text'];})
+							.classed("panel-tweet-body-text", true);
+					
 					var infoBlock = tweetDivs.append("p")
 									.classed("panel-tweet-info-block", true);
 
-			        infoBlock.append("img")
-			            .attr("src", imgRetweet)
-			           	.classed("panel-tweet-img-retweet", true);
-			        infoBlock.append("text")
-			            .text(function(d){
-			                return d['retweet_count'];
-			            })
-			            .classed("panel-tweet-text-retweet", true);
+				        infoBlock.append("img")
+				            .attr("src", imgRetweet)
+				           	.classed("panel-tweet-img-retweet", true);
+				        infoBlock.append("text")
+				            .text(function(d){
+				                return d['retweet_count'];
+				            })
+				            .classed("panel-tweet-text-retweet", true);
 
-					infoBlock.append("img")
-				            .attr("src", imgFav)
-				            .classed("panel-tweet-img-favorite", true);
-			       	infoBlock.append("text")
-			            .text(function(d){
-			                return d['favorite_count'];
-			            })
-					.classed("panel-tweet-text-favorite", true);
+						infoBlock.append("img")
+					            .attr("src", imgFav)
+					            .classed("panel-tweet-img-favorite", true);
+				       	infoBlock.append("text")
+				            .text(function(d){
+				                return d['favorite_count'];
+				            })
+						.classed("panel-tweet-text-favorite", true);
 
 				  })
 				  .fail(function() {
